71ca8ec3ec9c74e3169933401ba2f5ea2050d069
Rolando_cf <rolandodj83@gmail.com>
2024-10-07 19:15:26 +0200

    Ejercicios clase 4_1

---
+++ script.js
@@ -0,0 +1,10 @@
+/* let frutas =["Manzana", "Bananas"];
+console.log(frutas); // Manzana y Banana
+console.log(frutas.length);
+let first = frutas[0]; // Manzana
+let last = frutas[frutas.length -1]; // Banana */
+
+var frase = "Hola Mundo!";
+alert(frase);
+
+
