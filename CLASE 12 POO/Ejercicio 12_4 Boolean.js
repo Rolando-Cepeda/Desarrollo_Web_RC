df4f06cc851d94e9a6a955f50fc72a2d8c1bb105
Rolando_cf <125164239+Rolando-Cepeda@users.noreply.github.com>
2024-10-24 20:54:58 +0200

    Ejercicio con EMMET 12_7 y Terminar CSS

---
+++ CLASE 12 POO/Ejercicio 12_4 Boolean.js
@@ -0,0 +1,23 @@
+
+/* 
+Ejercicio
+Utilizando objetos Boolean realiza un programa que indique si un array de 6 elementos solicitado al usuario se encuentra ordenado de la siguiente forma (e1 > e3, e2 < e4 y e5 = e6)
+*/
+
+
+
+let user = alert("BIENVENIDO, DEBES INTRODUCIR 6 NÚMEROS AL AZAR")
+let e1 = parseInt(prompt("PRIMER NÚMERO:"));
+let e2 = parseInt(prompt("SEGUNDO NÚMERO:"));
+let e3 = parseInt(prompt("TERCER NÚMERO:"));
+let e4 = parseInt(prompt("CUARTO NÚMERO:"));
+let e5 = parseInt(prompt("QUINTO NÚMERO:"));
+let e6 = parseInt(prompt("SEXTO NÚMERO:"));
+
+console.log(typeof e1);
+console.log(typeof e22);
+
+
+
+
+
