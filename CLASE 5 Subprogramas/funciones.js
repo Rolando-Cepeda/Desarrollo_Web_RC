d86e79d1ce146744ad06396086537f8d17749fdd
Rolando_cf <rolandodj83@gmail.com>
2024-11-04 14:59:49 +0100

    Funciones 5_1

---
+++ CLASE 5 Subprogramas/funciones.js
@@ -0,0 +1,12 @@
+function holaRolando () {
+    alert("Hola Mundo")
+}
+
+function hiRolando() {
+    //return "Si Señor 6 veces.";
+    return  ["Matemáticas", " Química"] ;
+}
+
+function saludar (nombre) {
+    alert("Buenos días " + nombre + " que tengas un buen fin de semana");
+}
